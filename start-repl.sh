924c32dad12c3979491282b25a7056eb678be42e
brown@paradicsom.nyc.corp.google.com <brown@paradicsom.nyc.corp.google.com>
2009-10-01 13:33:16 -0400

    Fix small glitches.

--- start-repl.sh
+++ start-repl.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
-if [ $# < 2 ]; then
+if [ $# -lt 2 ]; then
     echo "Usage: start_repl.sh JAVA_BIN PORT"
+    exit 1
 fi
 
 JAR_FILES=`find third-party -name "*.jar" | tr '\n' :`
 JAVA_BIN=$1
 PORT=$2
 
-$JAVA_BIN -cp $JAR_FILES clojure.main /Users/ahyatt/src/wave-client-for-emacs/src/client.clj $PORT
+$JAVA_BIN -cp $JAR_FILES clojure.main ./src/client.clj $PORT
