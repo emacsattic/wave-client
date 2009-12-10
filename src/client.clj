b0575384b46ce9aff794404f02ddbbcd5b13f166
Andrew Hyatt <ahyatt@gmail.com>
2009-12-10 14:23:32 -0500

    Add method to get the data of a wave in the Clojure REPL.
    We may not yet be dealing with different wavelets correctly, but the main
    logic should work.

--- src/client.clj
+++ src/client.clj
@@ -17,6 +17,8 @@
        :doc "Wave Client Code - starts REPL on socket"}
   wave-for-emacs.client
   (:import (org.waveprotocol.wave.examples.fedone.waveclient.common ClientBackend ClientUtils))
+  (:import (org.waveprotocol.wave.model.document.operation DocInitializationCursor))
+  (:import (org.waveprotocol.wave.model.document.operation.impl InitializationCursorAdapter))
   (:use clojure.contrib.server-socket))
 
 (defn open-backend [user-at-domain server port]
@@ -72,6 +74,51 @@
 (defn new-wave []
   #^{:doc "Create a new wave"}
   (.createConversationWave backend))
+
+(defn nest-wavelet-data [l]
+  #^{:doc "Convert a linear output of text and tag beginning and endings
+           to a recursively nested format."}
+  (loop [accum '()
+         data-list l]
+    (if (empty? data-list)
+      accum
+      (let [head (first data-list)
+            new-accum
+            (cond
+                (string? head) (concat (butlast accum)
+                                       (list (concat (last accum)
+                                                     (list head))))
+              (vector? head) (concat accum
+                                     (list (list head)))
+              (= 'end head) (concat (butlast (butlast accum))
+                                    (concat (last (butlast accum))
+                                            (list (last accum))))
+              true (throw (new IllegalArgumentException
+                               (format "Unknown value: %s" head))))]
+        (recur new-accum (rest data-list))))))
+
+(defn get-wave [wave-id]
+  #^{:doc "Open a Wave given by WaveId instance WAVE-ID.  This return a
+           list of wavelets, each one which is a nested list of data.
+           The elements of this are strings and element groups with a
+           type symbol and attribute map, such as '(bold {} 'foo' 'bar)."}
+  (let [docmap (atom '())]
+    (doseq [wavelet (seq (.getWavelets (.getWave backend wave-id)))]
+      (doseq [doc-op (seq (.values (.getDocuments wavelet)))]
+        (.apply doc-op
+                (new InitializationCursorAdapter
+                     (proxy
+                         [DocInitializationCursor]
+                         []
+                       (characters [s]
+                                   (swap! docmap concat (list s)))
+                       (elementStart [type attr]
+                                     (swap! docmap concat
+                                            (list [(symbol type) attr])))
+                       (elementEnd []
+                                   (swap! docmap concat (list 'end)))
+                       (annotationBoundary [boundary-map]))))))
+    (nest-wavelet-data @docmap)))
 
 (let [port (new Integer (first *command-line-args*))]
   (clojure.contrib.server-socket/create-repl-server port))
