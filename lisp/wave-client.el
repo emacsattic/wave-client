5a891f21caa12435ed5f16b94571d4244bdae7e4
Andrew Hyatt <ahyatt@gmail.com>
2009-12-23 19:33:23 -0500

    First version of wave contents retrieval.  This should give us enough data to draw a wave on the UI.
    --HG--
    branch : direct-connect

--- lisp/wave-client.el
+++ lisp/wave-client.el
@@ -101,6 +101,36 @@ to the end, if given.  Uses `wave-client-domain'."
                       "/wave")
           (or path "/")))
 
+(defun wave-client-reset ()
+  "Clear the stored session and auth token.  Useful for when you
+  are switching domains."
+  (interactive)
+  (setq wave-client-auth-cookie nil)
+  (setq wave-client-session nil)
+  (setq wave-client-password nil))
+
+(defun wave-client-get-wave-raw (wave-id)
+  "Get the wave given from the WAVE-ID, as a plist data structure
+that is a direct conversion from the JSON."
+  (unless (and wave-client-auth-cookie wave-client-session)
+    ;; To re-fetch both auth and session
+    (wave-client-get-waves))
+  (save-excursion
+    (unwind-protect
+        (progn
+          (set-buffer
+           (wave-client-curl
+            (wave-client-get-url
+             (format "/wfe/fetch/%s/%s?v=3"
+                     (url-hexify-string wave-id)
+                     (plist-get (plist-get wave-client-session :userProfile)
+                                :id))) '()
+                                `(("WAVE" . ,wave-client-auth-cookie))))
+          (goto-char (point-min))
+          (re-search-forward "{")
+          (wave-client-json-read (substring (buffer-string) (- (point) 2))))
+      (wave-client-kill-current-process-buffer))))
+
 (defun wave-client-get-auth-cookie ()
   "Return the auth cookie for this user."
   (save-excursion
@@ -141,7 +171,8 @@ otherwise do nothing."
 buffer with the result."
   (let* ((buf (generate-new-buffer wave-client-process-buf-name))
          (retval
-          (apply 'call-process (append (list "curl" nil buf nil url "-f")
+          (apply 'call-process (append (list "curl" nil buf nil
+                                             url "-f" "-s")
                                        (mapcan (lambda (c)
                                                  (list "-d"
                                                        (concat
@@ -180,7 +211,7 @@ buffer with the result."
 the `wave-client-session' variable."
   (let ((auth-cookie (or wave-client-auth-cookie
                          (let ((cookie (wave-client-get-auth-cookie)))
-                           (setq wave-client-get-auth-cookie cookie)
+                           (setq wave-client-auth-cookie cookie)
                            cookie))))
     (save-excursion
       (unwind-protect
@@ -207,6 +238,82 @@ into the format defined by `wave-inbox'."
              (cons :author (plist-get wave :4))))
           (plist-get (plist-get wave-plist :p) :1)))
 
+(defun wave-client-extract-boundary (boundary)
+  "Extract information about the boundary"
+  (let ((result '(boundary)))
+    (if (plist-get boundary :1)
+        (add-to-list 'result 'empty) t)
+    (if (> (length (plist-get boundary :2)) 0)
+        (add-to-list 'result
+                     (list 'end (plist-get boundary :2)) t))
+    (if (> (length (plist-get boundary :3)) 0)
+        (add-to-list 'result
+                     (list 'change
+                           (mapcar
+                            (lambda (update)
+                              `((:key . ,(plist-get update :1))
+                                (:oldvalue . ,(plist-get update :2))
+                                (:newvalue . ,(plist-get update :3))))
+                              (plist-get boundary :3))) t))
+    result))
+
+(defun wave-client-extract-blip (blip-plist)
+  "Extract information about a single blip from the raw
+  BLIP-PLIST."
+  (list (cons :blip-id (plist-get blip-plist :1))
+        (cons :authors (plist-get blip-plist :7))
+        (cons :modified-time
+              (wave-client-extract-long (plist-get blip-plist :3)))
+        (cons :ops (mapcar
+                    (lambda (token)
+                      (let ((start-element (plist-get token :4))
+                            (text (plist-get token :2))
+                            (end-element (plist-get token :5))
+                            (boundary (plist-get token :1)))
+                        (cond (start-element
+                               (intern (plist-get start-element :1)))
+                              (text text)
+                              (end-element 'end)
+                              (boundary
+                               (wave-client-extract-boundary boundary))
+                              (t 'unknown))))
+                     (plist-get (plist-get blip-plist :16) :2)))
+        (cons :children (mapcar (lambda (child)
+                                  (plist-get child :6))
+              (plist-get blip-plist :5)))))
+
+(defun wave-client-extract-long (long)
+  "From a 2-byte long, extract a single long integer"
+  ;; TODO(ahyatt) Don't know how to do this yet
+  0)
+
+(defun wave-client-extract-wavelet (wavelet-plist)
+  "Extract information about a wavelet from the raw WAVE-PLIST,
+  transforming it into the format detined by `wave-get-wave'"
+  (let* ((wavelet (plist-get wavelet-plist :1))
+         (metadata (plist-get wavelet :1))
+         (blips (plist-get wavelet :2)))
+    (list (cons :participants (plist-get metadata :5))
+          (cons :creator (plist-get metadata :3))
+          (cons :wave-id (plist-get metadata :1))
+          (cons :wavelet-id (plist-get metadata :2))
+          (cons :creation-time (wave-client-extract-long
+                                (plist-get metadata :4)))
+          (cons :root-blip-id (plist-get metadata :6))
+          (cons :blips (mapcar
+                        (lambda (blip)
+                          (let ((extracted-blip
+                                 (wave-client-extract-blip blip)))
+                            (cons (cdr (assoc :blip-id extracted-blip))
+                                  extracted-blip)))
+                               blips)))))
+
+(defun wave-client-extract-wave (wave-plist)
+  "Extract information about a single wave from the raw
+  WAVE-PLIST, transforming it into the format defined by
+  `wave-get-wave'"
+  (mapcar 'wave-client-extract-wavelet (plist-get wave-plist :1)))
+
 ;; Functions for the Wave mode to use:
 
 (defun wave-inbox ()
@@ -215,6 +322,22 @@ each having a `:digest',`:author', and `id'.  The author is the
 first contributor to the first wavelet, which should be the
 person who contributed the wave."
   (wave-client-extract-waves (wave-client-get-waves)))
+
+(defun wave-get-wave (wave-id)
+  "Get a wave given a WAVE-ID.
+
+Format is a list of wavelets, each wavelet is an alist
+of :participants (a list), :creator (a string), :wave-id (string),
+:wavelet-id, :creation-time (integer), :root-blip-id (possibly
+nil, possibly a string of a blip id, and :blips, a list of blips.
+Each blip is a cons of blip id and alist
+of :blipid, :authors, :modified-time, and :ops (a list of
+operations), and :children, a list of child blip ids.  The ops
+are interned element starts (wavelet, blip, node, etc.),
+end (signifiying closing out the nearest element start),
+strings (content), and boundaries of lists starting with an 'end
+or 'change to end or change blip properties."
+  (wave-client-extract-wave (wave-client-get-wave-raw wave-id)))
 
 (provide 'wave-client)
 
